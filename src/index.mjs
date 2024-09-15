fc87f6aa10d8bb3a75a0a92849bbe4367ae55bd2
xＧＲＩＤ <34432992+ZufarNaufal@users.noreply.github.com>
2024-09-15 20:38:52 +0700

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Latihan Javascript!</h1>
+`;
---
+++ src/test-1.js
@@ -0,0 +1,6 @@
+var z, x;
+z = 5;
+x = 5;
+
+//hasil = z + x;
+console.log("result Of this Calculate", z + x);
