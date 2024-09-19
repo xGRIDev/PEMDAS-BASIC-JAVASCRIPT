93c1555ee58adec65a47a7da5fb5240f6fd5d4bb
xGRID <34432992+ZufarNaufal@users.noreply.github.com>
2024-09-19 02:12:00 +0000

    update

--- src/variable-example.js
+++ src/variable-example.js
@@ -1,5 +1,10 @@
-var x = 10;
-var y = 20;
+/*VAR EXAMPLE
+pada penjelasan dibawah adalah penerapan variable(var)
+untuk menampung sebuah nilai kedalam memory
+*/
+
+var x = 10; // deklarasikan / jabarkan value & key yang akan dimasukan
+var y = 20; // deklarasi variable
 
 console.log(x);
 console.log(y);
