7fd865278ce4679d72c1c371f1b69a8fc7c61bda
xGRID <34432992+ZufarNaufal@users.noreply.github.com>
2024-10-18 02:24:16 +0000

    UPDATE

--- src/VARIABLE/function-scope.js
+++ src/VARIABLE/function-scope.js
@@ -1,13 +1,13 @@
 function TestFunc() {
-    if(true){
-        var a = "Javascript";
-        let b = 'C++';
-        const c = ;
-        console.log(c);
-    }
-    console.log("outside if statement");
-    console.log(a);
-    console.log(b);
+  if (true) {
+    var a = "Javascript";
+    let b = "C++";
+    const c = "Python";
     console.log(c);
-
+  }
+  console.log("outside if statement");
+  console.log(a);
+  console.log(b);
+  console.log(c);
 }
+TestFunc();
