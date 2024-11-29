838f58110b4fb1eaf050d85e0b84d754943653d8
xGRID <34432992+xGRIDev@users.noreply.github.com>
2024-11-29 04:34:50 +0000

    UPDATE NEW

---
+++ src/Objects/object-example-1.js
@@ -0,0 +1,15 @@
+//CONTOH DASAR-OBJECTS TIPE data
+
+let info_user = {
+  name: "Anomaly",
+  age: 20,
+  address: "Ciomas Permai Kab.Bogor Kec.Ciomas",
+  gender: "male",
+  region: "indonesia",
+  nid: "aaa0233",
+};
+
+console.log(info_user);
+console.log(
+  `Halo nama saya ${info_user.name} tinggal di ${info_user.address}, Saya berusia ${info_user.age} dengan nid ${info_user.nid} `
+);
